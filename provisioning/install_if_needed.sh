02072607519ed432d447e8ab43bf7cb6d46375db
Fernando Ipar <fipar@acm.org>
2017-01-28 14:52:25 -0300

    added utility function to idempotently install rpms

---
+++ provisioning/install_if_needed.sh
@@ -0,0 +1,17 @@
+# idempotently install rpms
+# for example: 
+#  install_if_needed percona-release https://www.percona.com/redir/downloads/percona-release/redhat/latest/percona-release-0.1-4.noarch.rpm
+#  install_if_needed Percona-Server-MongoDB
+#
+install_if_needed()
+{
+   [ $# -eq 0 ] && {
+	echo "usage: install_if_needed <package-name> <package-url>">&2
+	exit 1
+   }
+   p=$1
+   url=$2
+   [ -z "$url" ] && url=$p
+   rpm -qa|grep $p >/dev/null && return 0 || yum -y install $url
+}
+
